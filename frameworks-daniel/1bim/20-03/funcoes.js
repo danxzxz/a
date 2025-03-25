7858fd7cc44bf0361eeaf2aa2678a04bf0212c50
dan <134729425+danxzxz@users.noreply.github.com>
2025-03-24 22:51:51 -0300

    Add files via upload

--- frameworks-daniel/1bim/20-03/funcoes.js
+++ frameworks-daniel/1bim/20-03/funcoes.js
@@ -4,22 +4,21 @@ var ctxConteudo;
 var ctxRodape;
 
 function configEstiloCabecalho() {
-    bg = document.getElementById("corFundo").value;
-    corFonte = document.getElementById("corFonte").value;
-    tamFonte = document.getElementById("tamFonte").value;
-
-    ctxCabecalho="#cabecalho{\nbackground-color:"+bg+";\n";
+    bg=document.getElementById("corFundo").value;
+    corFonte=document.getElementById("corFonte").value;
+    tamFonte=document.getElementById("tamFonte").value;
+    ctxCabecalho="#cabecalho{\n background-color:"+bg+";\n";
     ctxCabecalho+=" color:"+corFonte+";\n";
-    ctxCabecalho+= " font-size:"+tamFonte+"pt;\n}\n";
+    ctxCabecalho+=" font-size:"+tamFonte+"pt;\n}\n";
     return ctxCabecalho;
  }
 
 function configEstiloLinks() { 
-    corlink = document.getElementById("corLinks").value;
-    estiloLinks = document.querySelector('input[name="estiloLinks"]:checked').value;
-    ctxLinks = "a{\n color:"+corLink+";\n";
-    let aux = estiloLinks == "0"?"none":"underline";
-    ctxLinks += "text-decoration:" +aux+";\n}\n";
+    corLink=document.getElementById("corLinks").value;
+    estiloLinks=document.querySelector('input[name="estiloLinks"]:checked').value;
+    ctxLinks="a{\n color:"+corLink+";\n";
+    let aux=estiloLinks=="0"?"none":"underline";
+    ctxLinks+=" text-decoration:"+aux+";\n}\n";
     return ctxLinks;
 }
 
@@ -34,23 +33,23 @@ function configHTMLCabecalho() {
 }
 
 function gerarCodigo() {
-    //codigo para css
-    let codeCSS = document.querySelector("#codeCSS");
-    let css = configEstiloCabecalho();
-    css += configEstiloLinks();
-    codeCSS.value = css;
-
-    //codigo para HTML
-    let codeHTML = document.querySelector("#codeHTML");
-    ctxHTML = "<html>\n<head>\n" +
-        "<link rel='stylesheet' href='estilo.css'>\n" +
-        "<title>Minha Página</title>\n" +
+    //Cóigo para CSS
+    let codeCSS=document.querySelector("#codeCSS");
+    let css=configEstiloCabecalho();
+    css+=configEstiloLinks();
+    codeCSS.value=css;
+   //Código para HTML
+    let codeHTML=document.querySelector("#codeHTML");
+    ctxHTML="<html>\n<head>\n" +
+        "<link rel='stylesheet' href='estilo.css'>\n"+
+        "<title>Miha página</title>\n"+
         "</head>\n<body>" +
-        "<div id='cabecalho'>" + configHTMLCabecalho() + "</div>\n" +
-        "<nav id='links'>\n" + configHtmlLinks() + "\n</nav>\n" +
-        "<div id='conteudo'> </div>\n"
-    "</body>\n</html>";
-    codeHTML.value = ctxHTML;
+        "<div id='cabecalho'>"+configHTMLCabecalho()+"</div>\n" +
+        "<nav id='links'>\n"+configHtmlLinks()+"\n</nav>\n" +
+        "<div id='conteudo'></div>\n" +
+        "</body>\n</html>";
+    codeHTML.value=ctxHTML;
+
 }
 
 function download(campo, arquivo) { }
